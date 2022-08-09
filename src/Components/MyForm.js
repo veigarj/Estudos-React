ecc78264cf85091bf6bbaf124edbb13c6168c39e
Glauco Veiga <101466539+veigarj@users.noreply.github.com>
2022-08-09 00:05:23 -0300

    MyForm

--- src/Components/MyForm.js
+++ src/Components/MyForm.js
@@ -1,6 +1,10 @@
 import "./MyForm.css";
 
+import { useState } from 'react'
+
 const MyForm = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
   return (
     <div>
       {/* 1 - criacao de formulario */}
