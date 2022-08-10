d0f3024cb64b53f7b8ac4954fdd2a72c1e94fc9e
Glauco Veiga <101466539+veigarj@users.noreply.github.com>
2022-08-10 12:10:55 -0300

    Myform

--- src/Components/MyForm.js
+++ src/Components/MyForm.js
@@ -1,10 +1,18 @@
 import "./MyForm.css";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 const MyForm = () => {
-  const [name, setName] = useState()
-  const [email, setEmail] = useState()
+  // 3 Gerenciamento de Dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    console.log(e.target.value);
+  };
+
+  console.log(name);
+
   return (
     <div>
       {/* 1 - criacao de formulario */}
@@ -12,10 +20,23 @@ const MyForm = () => {
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite o nome" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o nome"
+            onChange={handleName}
+          />
         </div>
         <input type="submit" value="Enviar" />
       </form>
+
+      {/* 2 - Label envolvido em input */}
+
+      <label>
+        <span>E-mail:</span>
+        <input type="email" name="email" placeholder="Digite aqui seu email" />
+      </label>
+      <input type="submit" valeu="Enviar" />
     </div>
   );
 };
